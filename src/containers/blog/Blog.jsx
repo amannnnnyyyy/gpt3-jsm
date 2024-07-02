f0880a72a4a6697cfe8c0abeda53d9662f8a0bf0
amannnnnyyyy <mohaaman922@gmail.com>
2024-07-02 14:46:55 +0300

    blog section and articles

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import './blog.css'
+import {Article} from '../../components'
+import {blog01, blog02, blog03, blog04, blog05} from './imports'
 
 const Blog = () => {
+  const date = 'Sep 26, 2021'
+  const title = 'GPT-3 and Open  AI is the future. Let us explore how it is?'
+  const foot = 'Read Full Article'
   return (
-    <div>
-      Blog
+    <div className='gpt3__blog section__padding' id='blog'>
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening, 
+        We are blogging about it.</h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container_groupA'>
+          <Article imgUrl={blog01} date={date} title={title} foot={foot}/>
+        </div>
+        <div className='gpt3__blog-container_groupB'>
+        <Article imgUrl={blog02} date={date} title={title} foot={foot}/>
+        <Article imgUrl={blog03} date={date} title={title} foot={foot}/>
+        <Article imgUrl={blog04} date={date} title={title} foot={foot}/>
+        <Article imgUrl={blog05} date={date} title={title} foot={foot}/>
+        </div>
+      </div>
     </div>
   )
 }
